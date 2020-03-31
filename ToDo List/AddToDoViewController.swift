c74e4eb5199a9140fb355c4a0f8e322da48eea59
John Milligan <jonomilligan@gmail.com>
2020-03-31 22:19:17 +0200

    First commit for this project

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Peter Milligan on 2020/03/31.
+//  Copyright © 2020 John Milligan. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+        toDo.name = titleTextField.text!
+        toDo.important = importantSwitch.isOn
+        
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        
+        navigationController?.popViewController(animated: true)
+    }
+
+}
---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Peter Milligan on 2020/03/31.
+//  Copyright © 2020 John Milligan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
