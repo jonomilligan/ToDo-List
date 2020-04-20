93e3497b99e5ce05b480a9f6a33241d2b4c2131b
John Milligan <jonomilligan@gmail.com>
2020-04-20 18:25:29 +0200

    Final app

--- ToDo List/AddToDoViewController.swift
+++ ToDo List/AddToDoViewController.swift
@@ -11,30 +11,48 @@ import UIKit
 class AddToDoViewController: UIViewController {
     
     var previousVC = ToDoTableViewController()
-
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var importantSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
-        let toDo = ToDo()
-
-        if let nameUnwrapped = titleTextField.text {
-            toDo.name = nameUnwrapped
-            toDo.important = importantSwitch.isOn
+        /*let toDo = ToDo()
+         
+         if let nameUnwrapped = titleTextField.text {
+         toDo.name = nameUnwrapped
+         toDo.important = importantSwitch.isOn
+         
+         previousVC.toDos.append(toDo)
+         previousVC.tableView.reloadData()
+         
+         navigationController?.popViewController(animated: true)
+         
+         }*/
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
             
-            previousVC.toDos.append(toDo)
-            previousVC.tableView.reloadData()
+            let toDo = ToDoCoreData(entity: ToDoCoreData.entity(), insertInto: context)
+            
+            if let nameUnwrapped = titleTextField.text {
+                toDo.name = nameUnwrapped
+                toDo.important = importantSwitch.isOn
+            }
+            
+            try? context.save()
             
             navigationController?.popViewController(animated: true)
-        
         }
-
+        
+        
+        
+        
+        
     }
-
+    
 }
--- ToDo List/CompleteToDoViewController.swift
+++ ToDo List/CompleteToDoViewController.swift
@@ -11,27 +11,36 @@ import UIKit
 class CompleteToDoViewController: UIViewController {
     
     var previousVC = ToDoTableViewController()
-    var selectedToDo = ToDo()
+    var selectedToDo : ToDoCoreData?
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBAction func completeTapped(_ sender: Any) {
-        var index = 0
-        for toDo in previousVC.toDos{
-            if toDo.name == selectedToDo.name {
-                previousVC.toDos.remove(at: index)
-                previousVC.tableView.reloadData()
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let theToDo = selectedToDo {
+                context.delete(theToDo)
                 navigationController?.popViewController(animated: true)
-                break
             }
-            index += 1
         }
+//        var index = 0
+//        for toDo in previousVC.toDos{
+//            if toDo.name == selectedToDo.name {
+//                previousVC.toDos.remove(at: index)
+//                previousVC.tableView.reloadData()
+//                navigationController?.popViewController(animated: true)
+//                break
+//            }
+//            index += 1
+//        }
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = selectedToDo.name
-
+        if let theToDo = selectedToDo {
+            titleLabel.text = theToDo.name
+        }
+        
         // Do any additional setup after loading the view.
     }
 
